69f589dc0c9545431cfe216689d5e80398d1763c
Axolotle <nchesnais@mailoo.org>
2019-05-16 15:28:44 +0200

    add recursive calculation of the folded version of each original planet0

--- formats.py
+++ formats.py
@@ -6,35 +6,51 @@ def getJson(filename):
     with open(filename, 'r') as f:
         return load(f)
 
+def rectSize(surfaceArea):
+    # Define rectangle's size so height * width == surfaceArea
+    # and height / width == √2 == 1.4142135623730951
+    height = sqrt(surfaceArea * 1.4142135623730951)
+    return [surfaceArea / height, height]
+
 def defineFormats():
-    objects = getJson('surfaces.json')
-    formats = []
-
-    for object in objects:
+    for planet in getJson('surfaces.json'):
         # 'surface' is in km²
-        surface = object['surface']
-        # sqrt(2) == 1.4142135623730951
-        height = sqrt(surface * 1.4142135623730951)
-        width = surface / height
-        object['preciseSizeKm2'] = [width, height]
+        surface = planet['surface']
+        serie = planet['serie']
+        width, height = rectSize(surface)
+        planet['size_km'] = [width, height]
 
-        # turn kilometers into millimeter
+        # turn kilometers into millimeter and round to the nearest integrer
+        # Planet's paper format '0' surface area ~= planet surface area (rounded
+        # to the nearest millimeter) so height / width is now ~= √2
         height = round(height * 1000000)
         width = round(width * 1000000)
-        object['sizeMM'] = [width, height]
+        planet['rounded_size_mm'] = [width, height]
 
-        n = 0
-        while height > 1000:
+        serieNumber = 0
+        planet['formats'] = []
+        planet['formats'].append(
+            '{}0: {} × {} mm'.format(serie, width, height)
+        )
+
+        # Based on Earth format serie 'E', E49 seems to be the closest format
+        # to A0 and can be contained in it. We also choose to stop calculating
+        # formats after 59 iterations (E59 ~= A10).
+        while serieNumber < 59:
+            serieNumber += 1
             if height > width:
                 height = floor(height / 2)
+                planet['formats'].append(
+                    '{}{}: {} × {} mm'.format(serie, serieNumber, height, width)
+                )
             else:
                 width = floor(width / 2)
-            n += 1
-        object['number'] = str(n) + ' (' + str(width) + ', ' + str(height) +')'
-        formats.append(object)
+                planet['formats'].append(
+                    '{}{}: {} × {} mm'.format(serie, serieNumber, width, height)
+                )
 
-    with open('output.json', 'w') as f:
-        dump(formats, f, indent=2, separators=(',', ': '))
+        with open('series/' + serie + '.json', 'w') as f:
+            dump(planet, f, ensure_ascii=False, indent=2, separators=(',', ': '))
 
 if __name__ == '__main__':
     defineFormats()
