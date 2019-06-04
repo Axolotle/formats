f7e8e3a99c85ad1319a5a4abca075fbac48723be
Axolotle <nchesnais@mailoo.org>
2019-06-04 21:05:34 +0200

    add specimen generation

--- print.py
+++ print.py
@@ -7,7 +7,7 @@ from svgwrite.text import Text
 
 from utils import getJson, getText, km2mm
 
-def getNewDocument(name, width, height):
+def getNewDocument(width, height):
     return Drawing(
         size=(str(width) + 'mm', str(height) + 'mm'),
         viewBox='0 0 {} {}'.format(width, height),
@@ -21,9 +21,8 @@ def getEllipseSize(radius, baseFormat, a4equi):
         'polar': ratio * km2mm(radius[1])
     }
 
-def genCover(planet, marges):
-    [width, height] = planet['a4equi']['format']
-    doc = getNewDocument(planet['name'] + '-cover', width, height)
+def genCover(planet, width, height, marges):
+    doc = getNewDocument(width, height)
     doc.defs.add(Style(getText('stylesheet.css')))
     center = [width/2, height/2]
     # get scaled size of the planet radius
@@ -59,8 +58,39 @@ def genCover(planet, marges):
 
     return doc
 
+def genSpecimen(planet, width, height, marges, number):
+    doc = getNewDocument(width, height)
+    doc.defs.add(Style(getText('stylesheet.css')))
+    center = [width/2, height/2]
+    lines = Group()
+    doc.add(lines)
+    doc.add(Text(planet['serie'] + str(number), insert=center, class_='center f' + str(number)))
+
+    w = width
+    h = height
+    thickness = 1
+    number += 1
+    for n in range(1, 21):
+        if n % 2 != 0:
+            h /= 2
+            line = 'M{},{} h{}'.format(width - w, h, w)
+            textPos = [width - w / 2, h + h / 2]
+        else:
+            w /= 2
+            line = 'M{},{} v{}'.format(width - w, 0, h)
+            textPos = [width - w - w / 2, h / 2]
+        lines.add(Path(d=line, stroke_width=round(thickness, 3)))
+        doc.add(Text(planet['serie'] + str(number), insert=textPos, class_='center f' + str(number)))
+        number += 1
+        thickness *= 0.7071067811865476
+
+    return doc
+
 if __name__ == '__main__':
+    planet = getJson('series/earth.json')
     marges = [12.5, 17.625]
-    earth = getJson('series/earth.json')
-    cover = genCover(earth, marges)
-    cover.saveas('cover.svg', pretty=True)
+    [width, height] = planet['a4equi']['format']
+    cover = genCover(planet, width, height, marges)
+    cover.saveas('{}-cover.svg'.format(planet['name']), pretty=True)
+    format0 = genSpecimen(planet, width, height, marges, 0)
+    format0.saveas('{}-{}{}.svg'.format(planet['name'], planet['serie'], 0), pretty=True)
