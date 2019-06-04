2f1ac5c8ef24917c0cf9be020a174c9220e07c4c
Axolotle <nchesnais@mailoo.org>
2019-06-04 18:53:42 +0200

    replace radius lines by unique path

--- print.py
+++ print.py
@@ -2,6 +2,7 @@ from json import load
 from svgwrite import Drawing
 from svgwrite.container import Style, Group
 from svgwrite.shapes import Ellipse, Line, Rect
+from svgwrite.path import Path
 from svgwrite.text import Text
 
 from utils import getJson, getText, km2mm
@@ -15,8 +16,10 @@ def getNewDocument(name, width, height):
 
 def getEllipseSize(radius, baseFormat, a4equi):
     ratio = a4equi[1] / km2mm(baseFormat[1])
-    return [ratio * km2mm(radius[0]),
-            ratio * km2mm(radius[1])]
+    return {
+        'equa': ratio * km2mm(radius[0]),
+        'polar': ratio * km2mm(radius[1])
+    }
 
 def genCover(planet, marges):
     [width, height] = planet['a4equi']['format']
@@ -26,11 +29,14 @@ def genCover(planet, marges):
     # get scaled size of the planet radius
     ellRadius = getEllipseSize(planet['radius'], planet['size_km'], [width, height])
 
-    doc.add(Ellipse(center=center, r=ellRadius))
-    # planet's polar radius
-    doc.add(Line(start=center, end=[center[0], center[1] - ellRadius[1]]))
-    # planet's equatorial radius
-    doc.add(Line(start=center, end=[center[0] + ellRadius[0], center[1]]))
+    doc.add(Ellipse(center=center, r=[ellRadius['equa'], ellRadius['polar']]))
+    # planet's polar and equatorial radius
+    doc.add(Path(d='M{},{} v{} h{}'.format(
+        center[0],
+        center[1] - ellRadius['polar'],
+        ellRadius['polar'],
+        ellRadius['equa']
+    )))
     # title (greek caps)
     doc.add(Text(planet['serie'], insert=center, id='cover-text', class_='center'))
     doc.add(Text('1', insert=(center[0], marges[1] + 5), class_='center'))
@@ -47,7 +53,7 @@ def genCover(planet, marges):
     ))
     doc.add(Text(
         '{}0 ±=== {} SURFACE'.format(planet['serie'], planet['name'].upper()),
-        insert=(center[0], height - marges[1] - 3 - (center[1] - ellRadius[1] - marges[1]) / 2),
+        insert=(center[0], height - marges[1] - 3 - (center[1] - ellRadius['polar'] - marges[1]) / 2),
         class_='center'
     ))
 
